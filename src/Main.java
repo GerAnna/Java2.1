ae405dc7dbe2cf91c2642d565a7f98e15fd6d146
Anna Gergert <annya.gergert@gmail.com>
2021-11-28 20:05:08 +0300

    Miles added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+      int price;
+      price = 25_000;
+      int MileCost;
+      MileCost = 20;
+      int Mile;
+      Mile = price/MileCost;
+        System.out.println(Mile + " миль Вы получаете в подарок!");
 
+    }
 }
