cc9e1be0149ca28c8e8c0871f76aed6d29ca7a7d
Anna Gergert <annya.gergert@gmail.com>
2021-12-04 18:16:57 +0300

    name changed

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
       int price;
       price = 25_000;
-      int MileCost;
+      int mileCost;
       MileCost = 20;
       int Mile;
-      Mile = price/MileCost;
+      Mile = price/mileCost;
         System.out.println(Mile + " миль Вы получаете в подарок!");
 
     }
